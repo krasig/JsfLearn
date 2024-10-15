d097d5b4b56fa69eedbf042ec6be8167ee07abd6
KrasiG <krasi@lirex.com>
2024-10-15 15:09:06 +0300

    method with parameter

--- src/components/Methods.vue
+++ src/components/Methods.vue
@@ -9,11 +9,26 @@ const myMethod= ()=> {
   counter.value++;
 }
 
+const buttonIndex=ref(null);
+
+const clickedB=(index) =>{
+  console.log("Button clicked: " + index);
+  buttonIndex.value=index;
+}
+
 </script>
 
 <template>
 <h3>Mthods</h3>
   <button id="click-me" v-on:click="myMethod">ClickMe {{counter}}</button>
+
+  <p>Clicked button:{{ buttonIndex }}</p>
+
+  <ul>
+    <li v-for="(value,index) in 3" :key="index">
+      <button @click="clickedB(value)">{{value}}</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
