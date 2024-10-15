03024bb18828737790ebdfcc83c63b494b4dfd11
KrasiG <krasi@lirex.com>
2024-10-15 14:51:31 +0300

    simple method

---
+++ src/components/Methods.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import {ref} from "vue";
+
+const counter = ref(1);
+
+const myMethod= ()=> {
+  console.log("Method was clicked!!!"+counter.value);
+  counter.value++;
+}
+
+</script>
+
+<template>
+<h3>Mthods</h3>
+  <button id="click-me" v-on:click="myMethod">ClickMe {{counter}}</button>
+</template>
+
+<style scoped>
+
+</style>
