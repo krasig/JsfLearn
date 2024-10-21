9158d27a7e7bad26c56177ff8f96a6ef974d9193
mackg <krasig@gmail.com>
2024-10-21 23:35:23 +0300

    PropsValidate

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,23 @@ import Watcher from "@/components/Watcher.vue";
 import Asynchronous from "@/components/Asynchronous.vue";
 import Exercise206 from "@/components/Exercise206.vue";
 import Props from "@/components/Props.vue";
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import PropsValidate from "@/components/PropsValidate.vue";
 
 const input=ref(null);
 
+const config={
+  items:3,
+  content:"some content",
+}
 
 </script>
 
 <template>
-
 <input v-model="input"/>
   <main>
     <props greeting="Hello" :who="input"/>
+    <props-validate content="aaaa" :items="1" :config="config"/>
 <!--    <Exercise206/>-->
     <hr/>
 <!--    <Asynchronous/>-->
---
+++ src/components/PropsValidate.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+import {computed} from "vue";
+
+const props = defineProps({
+  items:{
+    type:Number,
+    required:true,
+  },
+  content:{
+    type:String,
+    required:true,
+  },
+  config:{
+    type:Object,
+    required:true,
+    validator:(value)=>{
+      return typeof value.items==='number' && typeof value.content ==='string'
+    }
+  }
+
+})
+
+const repetitions = computed(()=>{
+  return Array.from({length:props.config.items});
+})
+
+
+</script>
+
+<template>
+items={{items}}, content={{content}}
+  Config:items-{{config.items}},content={{config.content}}
+
+  <div>
+  <span v-for="(item,index) in repetitions" :key="index">
+      --{{config.content}}
+  </span>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
