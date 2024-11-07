ae3f82394f888b8a6e1028872a71aca2444de6df
KrasiG <krasi@lirex.com>
2024-11-07 18:28:36 +0200

    Countable

--- src/components/Countable.vue
+++ src/components/Countable.vue
@@ -1,17 +1,34 @@
 <script setup>
 
-import {ref} from "vue";
+import {onBeforeMount, onBeforeUnmount, onMounted, ref} from "vue";
 
+import Countable from 'countable';
 const mytext=ref('');
+const count=ref({
+  words:11,
+  paragraphs:0,
+  characters:0
+});
 
+onMounted(()=>{
+  Countable.on(mytext.value,(counter)=>{
+    console.log(counter.words)
+    count.value.words=counter.words;
+    count.value.paragraphs=counter.paragraphs;
+    count.value.characters=counter.characters;
+  })
+})
+onBeforeUnmount(()=>{
+  Countable.off(mytext.value);
+})
 
 </script>
 
 
 <template>
   <h2>Countable</h2>
-  <textarea v-model="mytext" cols="30" rows="7" ></textarea>
-  {{mytext}}
+  <textarea ref="mytext" cols="30" rows="7" ></textarea>
+  {{mytext}}:{{count.characters}}-{{count.words}}-{{count.paragraphs}}
 </template>
 
 <style scoped>
