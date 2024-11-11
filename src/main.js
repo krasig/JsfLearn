e9d47ad62ffcedb8eac9e8f31596616c63ef23e5
mackg <krasig@gmail.com>
2024-11-11 23:13:11 +0200

    plugin 1

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axiosPlugn from '@/plugins/axios'
 
-createApp(App).mount('#app')
+const app=createApp(App)
+    app.use(axiosPlugn)
+    app.mount('#app')
